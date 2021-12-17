fd8909c41cea9c0218fb678e34fb1771162169fd
ShahmarALiyev <saliyev2019@gmail.com>
2021-12-16 22:31:42 -0600

    add my name

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello world again</h1>
+
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Shahmar
         </a>
       </header>
     </div>
