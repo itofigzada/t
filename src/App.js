0c67802d42e437c29885b7992bf48b8f24bf0801
ShahmarALiyev <saliyev2019@gmail.com>
2021-12-17 12:17:09 -0600

    works

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Shahmar
+          Learn React with Shahmar and ismayil
         </a>
       </header>
     </div>
