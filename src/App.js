aa9eb20901c61b33cf6d6ec3935fe36ae8d084cc
Ismayil <75140722+itofigzada2019@users.noreply.github.com>
2021-12-19 17:11:42 -0600

    auyeeee

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          alaverga
+         oayeeeee
         </a>
         <h1> ALL Set</h1>
       </header>
